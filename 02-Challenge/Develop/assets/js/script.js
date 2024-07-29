121cd2ff5b16f91616957ace0348948daff5cf5d
Fin M <qbres333@hotmail.com>
2024-07-28 20:36:30 -0600

    edit challenge comments

--- 02-Challenge/Develop/assets/js/script.js
+++ 02-Challenge/Develop/assets/js/script.js
@@ -12,7 +12,8 @@ function createTaskCard(task) {
 
 }
 
-// Todo: create a function to render the task list and make cards draggable
+/* Todo: create a function to render the task list and 
+ make cards draggable */
 function renderTaskList() {
 
 }
@@ -27,12 +28,15 @@ function handleDeleteTask(event){
 
 }
 
-// Todo: create a function to handle dropping a task into a new status lane
+/* Todo: create a function to handle dropping a task into a 
+ new status lane */
 function handleDrop(event, ui) {
 
 }
 
-// Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
+/* Todo: when the page loads, render the task list, add event
+  listeners, make lanes droppable, and make the due date 
+ field a date picker */
 $(document).ready(function () {
 
 });
