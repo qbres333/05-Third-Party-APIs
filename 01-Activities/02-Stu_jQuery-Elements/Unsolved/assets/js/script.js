5172d19cc6d3920fc59243791ad382f3bd8d1a02
Fin M <qbres333@hotmail.com>
2024-07-29 19:15:44 -0600

    class activities

--- 01-Activities/02-Stu_jQuery-Elements/Unsolved/assets/js/script.js
+++ 01-Activities/02-Stu_jQuery-Elements/Unsolved/assets/js/script.js
@@ -1,7 +1,10 @@
+
 /* TODO: Select the existing DOM element, <div id="root">, 
 and assign to a new variable */
 // // Hint: Visit the documentation https://api.jquery.com/
-const rootEl = $('#root');
+const rootEl = $('root');
+// const rootEl = document.getElementById("root");
+
 
 /* TODO: Create a '<p>' element that will store an author's 
 name and assign that created element to a new variable */
@@ -9,27 +12,29 @@ const authorName = $('<p>');
 
 /* TODO: Add the following text to the newly created '<p>' 
 element: "~ Carol Dweck" */
-authorName.text('~ Carol Dweck');
+authorName.textContent = "Carol Dweck";
+// authorName.text('~ Carol Dweck');
 
 /* TODO: Add the class 'plain' to the author element */
-authorName.addClass('plain');
+authorName.attr('class', 'plain');
 
-/* TODO: Create a new '<h1>' element that will store an author's quote and 
-assign it to new variable */
+/* TODO: Create a new '<h1>' element that will store an author's 
+quote and assign it to new variable */
 const authorQuote = $('<h1>');
 
 /* TODO: Add the following quote text to the quote element, "Love 
-Challenges, Be Intrigued by Mistakes, Enjoy Effort, and Keep Learning."
-authorQuote.text('Love Challenges, Be Intrigued by Mistakes, Enjoy 
-// Effort, and Keep Learning.'); */
+Challenges, Be Intrigued by Mistakes, Enjoy Effort, and Keep Learning." */
+authorQuote.text('Love Challenges, Be Intrigued by Mistakes, Enjoy Effort, and Keep Learning.'); 
 
-// // TODO: Apply the class 'fancy' to the quote element
+/* TODO: Apply the class 'fancy' to the quote element */
 authorQuote.addClass('fancy');
 
-// // TODO: Append the author element to the quote element
+/* TODO: Append the authorName element to the quote element */
 authorQuote.append(authorName);
 
 /* TODO: Append the quote element to the HTML element with an ID 
 of 'root' in 'index.html' */
 rootEl.append(authorQuote);
 
+console.log(authorName);
+console.log(authorQuote);
